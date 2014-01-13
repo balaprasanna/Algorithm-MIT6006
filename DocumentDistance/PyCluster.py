26d4f4dd38634ae724dd56466e33f945e52e2235
zymfrank <zymfrank@qq.com>
2014-01-13 16:04:14 +0800

    add names for dict

--- DocumentDistance/PyCluster.py
+++ DocumentDistance/PyCluster.py
@@ -8,8 +8,10 @@ class Cluster():
     def __init__(self,id=0):
         self.id=id
         self.points=[]
-        self.birth=0
         self.gender=''
+        self.birth=[]
+        self.stkid=[]
+
 
 
 def dbscan(data,eps,min_pts=1):
@@ -20,7 +22,7 @@ def dbscan(data,eps,min_pts=1):
         #for each unvisited points in dataset
         if point[0] not in visited:
             visited[point[0]]=True
-            neighbors=get_neighbors(point,data,eps)
+            neighbors=get_neighbors(point,data,eps,clusters)
             if len(neighbors)<=min_pts:
                 cluster_id+=1
                 c=Cluster(cluster_id)
@@ -38,7 +40,7 @@ def expand_cluster(point,data,neighbors,visited,c,clusters,eps):
     for neighbor_point in neighbors:
         if neighbor_point[0] not in visited:
             visited[neighbor_point[0]]=True
-            new_neighbors=get_neighbors(neighbor_point,data,eps)
+            new_neighbors=get_neighbors(neighbor_point,data,eps,clusters)
             join_neighbors(neighbors,new_neighbors)
         if not find_in_cluster(neighbor_point,clusters):
             c.points.append(neighbor_point)
@@ -56,28 +58,32 @@ def find_in_cluster(point,clusters):
             return True
     return False
 
-def get_neighbors(point_a,data,eps):
+def get_neighbors(point_a,data,eps,clusters):
     """
-    neighbors=[id1,id2,...]
+    neighbors=[point1,point2,point3...]
     """
     neighbors=[point_a]
     for point_b in data:
-        if point_a[0]!=point_b[0]:
+        if point_a[0]!=point_b[0] and not find_in_cluster(point_b,clusters):
             distance=DocumentDistance.get_distance(point_a[1],point_b[1])
             if point_a[2]!=point_b[2]:
                 distance+=0.45
+            if point_a[4]==point_b[4]:
+                distance-=0.1
             if distance<=eps:
                 neighbors.append(point_b)
     return neighbors
 
 def compute_cluster(cluster):
-    birth_dict={}
     points=cluster.points
     cluster.gender=points[0][2]
     for point in points:
         birth=point[3]
-        birth_dict[birth]=birth_dict.get(birth,0)+1
-    cluster.birth=get_mode(birth_dict)
+        stkid=point[4]
+        if birth not in cluster.birth:
+            cluster.birth.append(birth)
+        if stkid not in cluster.stkid:
+            cluster.stkid.append(stkid)
 
 def get_mode(dict):
     max_key=max(dict,key=dict.get)
@@ -85,12 +91,16 @@ def get_mode(dict):
 
 def scan_other_points(data,clusters):
     visited={}
-    cluster_id=clusters[len(clusters)-1].id
+    if clusters:
+        cluster_id=clusters[len(clusters)-1].id
+    else:
+        cluster_id=0
     for point in data:
         gender=point[2]
         birth=point[3]
+        stkid=point[4]
         for cluster in clusters:
-            if gender==cluster.gender and birth==cluster.birth:
+            if gender==cluster.gender and birth in cluster.birth and stkid in cluster.stkid:
                 cluster.points.append(point)
                 visited[point[0]]=True
                 break
@@ -103,7 +113,7 @@ def scan_other_points(data,clusters):
 
 def scan_similar_names(data):
     '''
-    data=[(id,cv,gender,birth,catagory),()...]
+    data=[(id,cv,gender,birth,stkid,catagory),()...]
     '''
     data_with_cv=[]
     data_without_cv=[]
@@ -112,7 +122,7 @@ def scan_similar_names(data):
             data_without_cv.append(d)
         else:
             data_with_cv.append(d)
-    clusters=dbscan(data_with_cv,0.55)
+    clusters=dbscan(data_with_cv,0.6)
     for cluster in clusters:
         compute_cluster(cluster)
     scan_other_points(data_without_cv,clusters)
